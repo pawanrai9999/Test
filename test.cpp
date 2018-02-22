ffe351ac969df7589c7c7d929686e007c14882d0
pawanrai9999 <pawanrai9999@gmail.com>
2018-02-22 18:54:55 +0530

    Init commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Enter a digit: ";
+	double inp, rel;
+	cin >> inp;
+	rel = inp + 3;
+	cout << inp << " + 3 :" << rel;
+	return 0;
+}
